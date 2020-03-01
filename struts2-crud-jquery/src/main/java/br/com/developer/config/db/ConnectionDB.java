a9605a5e691d1b584230a560d69f5ad82bbdf5c2
Rafael Barros <rafaelbarros.df@gmail.com>
2020-03-01 01:10:21 -0300

    :construction: Ajuste config db

--- struts2-crud-jquery/src/main/java/br/com/developer/config/db/ConnectionDB.java
+++ struts2-crud-jquery/src/main/java/br/com/developer/config/db/ConnectionDB.java
@@ -9,14 +9,15 @@ public class ConnectionDB {
 	private static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
 	private static final String DB_USER = "root";
 	private static final String DB_PASSWORD = "root";
-	private static final String URL_JDBC = "jdbc:mysql://172.18.0.1:3326/struts2_crud_jquery";
+	private static final String DB_DATABASE = "struts2_crud_jquery";
+	private static final String DB_URL_JDBC = "jdbc:mysql://172.18.0.1:3326";
 	private static final String ERROR_DRIVER = "O driver não foi carregado!";
 	
 	public static Connection getConnetion() {
 		Connection con = null;
 		try {
 			Class.forName(MYSQL_DRIVER);
-			con = DriverManager.getConnection(URL_JDBC,DB_USER,DB_PASSWORD);
+			con = DriverManager.getConnection(DB_URL_JDBC.concat("/").concat(DB_DATABASE),DB_USER,DB_PASSWORD);
 		} catch (SQLException | ClassNotFoundException e) {
 			throw new RuntimeException(ERROR_DRIVER);
 		}
