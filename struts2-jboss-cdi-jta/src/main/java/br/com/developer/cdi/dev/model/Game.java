7bfc49755c86ce28c8a9160db6c7dc2c7e188f3d
Rafael Barros <rafaelbarros.df@gmail.com>
2020-05-10 23:57:35 -0300

    :construction: Criando model

---
+++ struts2-jboss-cdi-jta/src/main/java/br/com/developer/cdi/dev/model/Game.java
@@ -0,0 +1,20 @@
+package br.com.developer.cdi.dev.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+@ToString
+public class Game implements Serializable {
+
+    @Getter
+    @Setter
+    private String name;
+}
