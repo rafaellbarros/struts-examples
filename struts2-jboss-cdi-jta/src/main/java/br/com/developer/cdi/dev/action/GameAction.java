0969ad17b0119bc2706926cd48f87578ee1c36f4
Rafael Barros <rafaelbarros.df@gmail.com>
2020-05-12 01:20:49 -0300

    :construction: Implementando action

--- struts2-jboss-cdi-jta/src/main/java/br/com/developer/cdi/dev/action/GameAction.java
+++ struts2-jboss-cdi-jta/src/main/java/br/com/developer/cdi/dev/action/GameAction.java
@@ -1,28 +1,45 @@
 package br.com.developer.cdi.dev.action;
 
 import br.com.developer.cdi.dev.model.Game;
+import br.com.developer.model.entity.Cliente;
+import br.com.developer.service.ClienteService;
 import com.opensymphony.xwork2.ActionSupport;
 import lombok.Getter;
-import lombok.extern.log4j.Log4j2;
 
 import javax.inject.Inject;
+import java.util.List;
 
-@Log4j2
-@Getter
 public class GameAction extends ActionSupport {
 
     @Inject
     Game game;
 
+    @Inject
+    ClienteService service;
+
+    @Getter
+    private List<Cliente> clientes;
+
     public GameAction() {
         // System.out.println("******* " + game.hello() + " *******") ;
-       log.info("******* Init  GameAction *******"); ;
+        System.out.println("******* Init  GameAction *******");
     }
 
     @Override
     public String execute() throws Exception {
         game.setName("Mortal XI");
         System.out.println(game.toString());
+
+        this.clientes = service.buscarTodos();
+
+        Cliente cliente = new Cliente();
+        cliente.setNome("Xablau Shelby");
+        cliente.setIdade(30);
+        cliente.setProfissao("Muleque da Zoeira");
+        cliente.setSexo("M");
+
+        // service.salvar(cliente);
+
         return SUCCESS;
     }
 }
