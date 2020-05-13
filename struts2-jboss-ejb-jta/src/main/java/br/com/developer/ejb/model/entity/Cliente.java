b95c3be011f69159440fcdd097b1b56ab0ce21b8
Rafael Barros <rafaelbarros.df@gmail.com>
2020-05-13 06:13:40 -0300

    :construction: Criando entity

---
+++ struts2-jboss-ejb-jta/src/main/java/br/com/developer/ejb/model/entity/Cliente.java
@@ -0,0 +1,22 @@
+package br.com.developer.ejb.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@EqualsAndHashCode(of = "codigo")
+@Table(name="tb_cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long codigo;
+    private String nome;
+    private Integer idade;
+    private String sexo;
+    private String profissao;
+
+}
