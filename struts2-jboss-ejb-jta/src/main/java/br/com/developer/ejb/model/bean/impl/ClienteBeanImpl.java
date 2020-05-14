a0b04eaa513d0181e98800536b1b8f1daeafbf8e
Rafael Barros <rafaelbarros.df@gmail.com>
2020-05-14 09:19:14 -0300

    :construction: Comentando

--- struts2-jboss-ejb-jta/src/main/java/br/com/developer/ejb/model/bean/impl/ClienteBeanImpl.java
+++ struts2-jboss-ejb-jta/src/main/java/br/com/developer/ejb/model/bean/impl/ClienteBeanImpl.java
@@ -1,29 +1,21 @@
 package br.com.developer.ejb.model.bean.impl;
 
-import br.com.developer.ejb.model.bean.ClienteBean;
-import br.com.developer.ejb.model.entity.Cliente;
+import java.io.Serializable;
 
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.transaction.Transactional;
-import java.util.List;
+public class ClienteBeanImpl implements Serializable {
 
-@Stateless
-public class ClienteBeanImpl implements ClienteBean {
+    /*
 
     @PersistenceContext(unitName="projetoJbossPU")
     private EntityManager em;
 
-    @Override
     @Transactional
     public boolean create(Cliente cliente) {
         em.persist(cliente);
         return true;
     }
 
-    @Override
+
     public Cliente getById(Long id) {
         Query query = em.createQuery("SELECT c FROM Cliente c WHERE c.id = :ID");
         query.setParameter("ID", id);
@@ -31,23 +23,25 @@ public class ClienteBeanImpl implements ClienteBean {
         return cliente;
     }
 
-    @Override
+
     public List<Cliente> findAll() {
         Query query = em.createQuery("SELECT e FROM Cliente e");
         List<Cliente> clientes = query.getResultList();
         return clientes;
     }
 
-    @Override
+
     @Transactional
     public void update(Cliente cliente) {
         em.merge(cliente);
     }
 
-    @Override
     public void delete(Long id) {
         Query query = em.createQuery("DELETE FROM Cliente c WHERE c.id = :ID");
         query.setParameter("ID", id);
         query.executeUpdate();
     }
+
+    */
+
 }
