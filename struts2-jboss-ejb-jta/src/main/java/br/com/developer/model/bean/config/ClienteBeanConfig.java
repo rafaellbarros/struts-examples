b35f4ee7d43b7f722410bfd0844e4eb709fe742f
Rafael Barros <rafaelbarros.df@gmail.com>
2020-05-14 21:04:27 -0300

    :construction: Criando config

---
+++ struts2-jboss-ejb-jta/src/main/java/br/com/developer/model/bean/config/ClienteBeanConfig.java
@@ -0,0 +1,7 @@
+package br.com.developer.model.bean.config;
+
+import br.com.developer.model.bean.remote.ClienteRemote;
+import br.com.developer.model.bean.remote.LookupDefault;
+
+public class ClienteBeanConfig implements LookupDefault<ClienteRemote> {
+}
