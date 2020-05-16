0e120d587e33f034a65128e7246da76cdb834fdb
Rafael Barros <rafaelbarros.df@gmail.com>
2020-05-16 13:32:11 -0300

    :construction: Criando e implementando ClienteService

--- struts2-jboss-ejb-jta/src/main/java/br/com/developer/service/ClienteService.java
+++ struts2-jboss-ejb-jta/src/main/java/br/com/developer/service/ClienteService.java
@@ -1,22 +1,52 @@
 package br.com.developer.service;
 
 import br.com.developer.model.entity.Cliente;
+import br.com.developer.repository.ClienteRepository;
 
-import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
 import java.util.List;
 
-@Local
-public interface ClienteService {
+/**
+ * @author  Rafael Barros <rafaelbarros.df@gmail.com>
+ */
 
-    boolean create(Cliente cliente);
+@Stateless
+public class ClienteService {
 
-    Cliente getById(Long id);
+    @Inject
+    private ClienteRepository repository;
 
-    List<Cliente> findAll();
+    public Cliente obterPorId(Long id) {
+        return repository.findById(id);
+    }
 
-    void update(Cliente cliente);
+    public List<Cliente> obterTodos() {
+        return repository.findAll();
+    }
 
-    void delete(Long id);
+    @Transactional
+    public boolean incluir(Cliente cliente) {
+        return repository.create(cliente);
+    }
 
-    void delete(Cliente cliente);
+    @Transactional
+    public boolean alterar(Cliente cliente) {
+        return repository.update(cliente);
+    }
+
+    @Transactional
+    public void excluirPorId(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Transactional
+    public void excluir(Cliente cliente) {
+        repository.delete(cliente);
+    }
+
+    public Long contarTodos() {
+        return repository.countAll();
+    }
 }
