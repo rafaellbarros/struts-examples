f5676d059084c80543b3e8e760d8b786680ad9c1
Rafael Barros <rafaelbarros.df@gmail.com>
2020-05-22 03:25:25 -0300

    :construction: Criando model usuario

---
+++ struts2-jobss-spring5/src/main/java/br/com/developer/model/Usuario.java
@@ -0,0 +1,18 @@
+package br.com.developer.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author  Rafael Barros <rafaelbarros.df@gmail.com>
+ */
+
+@Getter
+@Setter
+@ToString
+public class Usuario {
+
+    private String username;
+
+}
